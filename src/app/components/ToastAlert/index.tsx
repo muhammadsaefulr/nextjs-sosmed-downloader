0346f52d39001d6d4f9f7d9e1ede69c5ae5f0c5c
saepulid <saepull.id@gmail.com>
2024-06-26 19:53:23 +0700

    update: fix typerror

--- src/app/components/ToastAlert/index.tsx
+++ src/app/components/ToastAlert/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export const ToastAlert = ({ message }) => {
+interface ToastAlertProps {
+    message: string;
+  }
+
+export const ToastAlert: React.FC<ToastAlertProps> = ({ message }) => {
   return (
     <div className="fixed bottom-4 right-4 bg-green-500 text-white px-6 py-3 rounded-md shadow-lg">
       {message}
